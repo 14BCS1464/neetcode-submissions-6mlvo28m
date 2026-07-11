0fe07b58e44f0848e7c9058ae114be42a45acaf9
14BCS1464 <prajapatisunil135@gmail.com>
2026-07-11 21:30:04 +0530

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(mat) {
+        let n = mat.length;
+        for (let i = 0; i <= n - 1; i++) {
+            for (let j = i + 1; j <= n - 1; j++) {
+                [mat[i][j], mat[j][i]] = [mat[j][i], mat[i][j]];
+            }
+        }
+
+         for (let i = 0; i <= n - 1; i++) {
+    let start = 0;
+    let end = mat[0].length - 1;
+    while (start <= end) {
+     [ mat[i][start],mat[i][end]]=[mat[i][end],mat[i][start]]
+      start++
+      end--
+    }
+  }
+  return mat
+    }
+}
