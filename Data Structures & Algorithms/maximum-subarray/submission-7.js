d4d7436820757c8221c4632bdbf75318a98cf745
14BCS1464 <prajapatisunil135@gmail.com>
2026-07-11 22:41:17 +0530

    Add: maximum-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-subarray/submission-7.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let sum = 0;
+        let maxSum = -Infinity;
+
+        for (let i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            maxSum = Math.max(maxSum, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
