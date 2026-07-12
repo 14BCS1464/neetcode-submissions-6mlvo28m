2c4ef4224c20dba4849659bdd9d57f5d28416955
14BCS1464 <prajapatisunil135@gmail.com>
2026-07-12 10:11:35 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        let totalGas = 0;
+        let totalCost = 0;
+
+        let start = 0;
+        let currentGas = 0;
+
+        for (let i = 0; i <= gas.length - 1; i++) {
+            let diff = gas[i] - cost[i];
+            totalGas += gas[i];
+            totalCost += cost[i];
+
+            currentGas += diff;
+
+            if (currentGas < 0) {
+                start = i+1;
+                currentGas = 0;
+            }
+        }
+        if (totalGas < totalCost) return -1;
+        return start;
+    }
+}
